8a4ebbfd9d868e8caf04547b99a6a5ab7dd84286
Paul Poley <141367555+baylorpaul@users.noreply.github.com>
2025-06-12 11:46:12 -0500

    @JsonValue, WebAuthn4J, and Micronaut Serialization: Resolve WebAuthn4j "@JsonValue" usage with Micronaut Serialization issue: The Micronaut Serialization docs mention that it has "unsupported members: value" for @JsonValue, but that likely means the member in the @JsonValue annotation, not if @JsonValue is used on a member named "value", like most of the WebAuthn4j usages. So it seems like there's just a bug in Micronaut Serialization. For now, using JsonInclude.Include.ALWAYS on each wrapper class with a @Nullable value that uses @JsonValue on one of its members resolves the issue for our cases. See https://micronaut-projects.github.io/micronaut-serialization/latest/guide/#jacksonAnnotations

--- library/src/main/java/io/github/baylorpaul/webauthn4jmicronaut/WebAuthn4jSerdeConfig.java
+++ library/src/main/java/io/github/baylorpaul/webauthn4jmicronaut/WebAuthn4jSerdeConfig.java
@@ -1,45 +1,50 @@
 package io.github.baylorpaul.webauthn4jmicronaut;
 
 import com.webauthn4j.data.*;
+import com.webauthn4j.data.attestation.authenticator.*;
 import com.webauthn4j.data.attestation.statement.COSEAlgorithmIdentifier;
 import com.webauthn4j.data.client.challenge.DefaultChallenge;
 import com.webauthn4j.data.extension.client.AuthenticationExtensionsClientInputs;
 import com.webauthn4j.data.extension.client.AuthenticationExtensionsClientOutputs;
+import com.webauthn4j.data.jws.JWSHeader;
 import io.github.baylorpaul.webauthn4jmicronaut.dto.api.security.serialization.JsonIncludeAlwaysMixin;
 import io.github.baylorpaul.webauthn4jmicronaut.dto.api.security.serialization.PasskeyEntityByteArrayIdMixin;
 import io.micronaut.serde.annotation.SerdeImport;
 
 @SerdeImport.Repeated({
-		@SerdeImport(PublicKeyCredentialCreationOptions.class),
-		@SerdeImport(PublicKeyCredentialRpEntity.class),
-		@SerdeImport(
-				value = PublicKeyCredentialUserEntity.class,
-				// Serialize the byte array ID to a Base64Url string, instead of a JSON number[]
-				mixin = PasskeyEntityByteArrayIdMixin.class
-		),
-		@SerdeImport(
-				value = PublicKeyCredentialDescriptor.class,
-				// Serialize the byte array ID to a Base64Url string, instead of a JSON number[]
-				mixin = PasskeyEntityByteArrayIdMixin.class
-		),
-		@SerdeImport(DefaultChallenge.class),
-		@SerdeImport(PublicKeyCredentialParameters.class),
-		@SerdeImport(PublicKeyCredentialType.class),
-		@SerdeImport(COSEAlgorithmIdentifier.class),
-		@SerdeImport(
-				value = AuthenticatorSelectionCriteria.class,
-// TODO apply this mixin to more wrapping classes where the object may be null, and the class has a @JsonValue
-				mixin = JsonIncludeAlwaysMixin.class
-		),
-		@SerdeImport(PublicKeyCredentialHints.class),
-		@SerdeImport(AuthenticatorAttachment.class),
-		@SerdeImport(AuthenticatorTransport.class),
+		// PasskeyEntityByteArrayIdMixin - Serialize the byte array ID to a Base64Url string, instead of a JSON number[]
+		@SerdeImport(value = PublicKeyCredentialDescriptor.class, mixin = PasskeyEntityByteArrayIdMixin.class),
+		@SerdeImport(value = PublicKeyCredentialUserEntity.class, mixin = PasskeyEntityByteArrayIdMixin.class),
+
+		// JsonIncludeAlwaysMixin - fix an issue with Micronaut Serialization where one of the members is null, and its
+		// implementing class uses @JsonValue on one of its fields. This mixin is not necessarily needed if the member
+		// is a nullable Collection of such an instance, such as `@Nullable Set<AuthenticatorTransport> transports`
+		//@SerdeImport(value = AbstractCOSEKey.class, mixin = JsonIncludeAlwaysMixin.class),
+		@SerdeImport(value = AuthenticatorAttestationResponse.class, mixin = JsonIncludeAlwaysMixin.class),
+		@SerdeImport(value = AuthenticatorSelectionCriteria.class, mixin = JsonIncludeAlwaysMixin.class),
+		//@SerdeImport(value = COSEKey.class, mixin = JsonIncludeAlwaysMixin.class),
+		@SerdeImport(value = EC2COSEKey.class, mixin = JsonIncludeAlwaysMixin.class),
+		@SerdeImport(value = EdDSACOSEKey.class, mixin = JsonIncludeAlwaysMixin.class),
+		@SerdeImport(value = JWSHeader.class, mixin = JsonIncludeAlwaysMixin.class),
+		@SerdeImport(value = PublicKeyCredential.class, mixin = JsonIncludeAlwaysMixin.class),
+		@SerdeImport(value = PublicKeyCredentialCreationOptions.class, mixin = JsonIncludeAlwaysMixin.class),
+		@SerdeImport(value = PublicKeyCredentialRequestOptions.class, mixin = JsonIncludeAlwaysMixin.class),
+		@SerdeImport(value = RSACOSEKey.class, mixin = JsonIncludeAlwaysMixin.class),
+
+		// No mixin
+		@SerdeImport(AttestationConveyancePreference.class),
 		@SerdeImport(AuthenticationExtensionsClientInputs.class),
 		@SerdeImport(AuthenticationExtensionsClientOutputs.class),
+		@SerdeImport(AuthenticatorAttachment.class),
+		@SerdeImport(AuthenticatorTransport.class),
+		@SerdeImport(COSEAlgorithmIdentifier.class),
+		@SerdeImport(DefaultChallenge.class),
+		@SerdeImport(PublicKeyCredentialHints.class),
+		@SerdeImport(PublicKeyCredentialParameters.class),
+		@SerdeImport(PublicKeyCredentialRpEntity.class),
+		@SerdeImport(PublicKeyCredentialType.class),
 		@SerdeImport(ResidentKeyRequirement.class),
-		@SerdeImport(UserVerificationRequirement.class),
-		@SerdeImport(AttestationConveyancePreference.class),
-		@SerdeImport(PublicKeyCredentialRequestOptions.class)
+		@SerdeImport(UserVerificationRequirement.class)
 })
 public class WebAuthn4jSerdeConfig {
 }
