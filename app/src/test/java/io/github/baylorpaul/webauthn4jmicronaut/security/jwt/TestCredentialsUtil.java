4a48fb98f1c12c89725f7a5c559157e8989940d2
Paul Poley <141367555+baylorpaul@users.noreply.github.com>
2025-06-20 16:24:25 -0500

    TestCredentialUtil: Rename a method to make it clear the credentials are password-specific. Separate part of createTestCreds() into a saveTestUserIfNotExists() method in case some callers only need the user ID, and not also an access token.

--- app/src/test/java/io/github/baylorpaul/webauthn4jmicronaut/security/jwt/TestCredentialsUtil.java
+++ app/src/test/java/io/github/baylorpaul/webauthn4jmicronaut/security/jwt/TestCredentialsUtil.java
@@ -26,8 +26,6 @@ import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 import org.junit.jupiter.api.Assertions;
 
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-
 @Singleton
 public class TestCredentialsUtil {
 
@@ -59,17 +57,24 @@ public class TestCredentialsUtil {
 		return userRestService.createUser(email, null, null);
 	}
 
-	public TestCreds createTestCreds() {
+	/**
+	 * @return the user ID
+	 */
+	public long saveTestUserIfNotExists() {
 		String email = EmailUtil.formatEmailAddress(TEST_EMAIL);
 		userRepo.saveUserIfNotExists(email, TEST_NAME);
 		long userId = userRepo.findByEmail(email).map(User::getId).orElse(-1L).longValue();
-		assertNotEquals(-1L, userId);
+		Assertions.assertTrue(userId > 0L);
+		return userId;
+	}
 
-		String accessToken = createAccessToken(TEST_EMAIL, TEST_PASSWORD);
+	public TestCreds createTestCreds() {
+		long userId = saveTestUserIfNotExists();
+		String accessToken = createAccessTokenWithPassword(TEST_EMAIL, TEST_PASSWORD);
 		return new TestCreds(userId, accessToken);
 	}
 
-	public String createAccessToken(String email, String password) {
+	public String createAccessTokenWithPassword(String email, String password) {
 		UsernamePasswordCredentials creds = new UsernamePasswordCredentials(email, password);
 		HttpRequest<?> request = HttpRequest.POST("/login", creds);
 		BearerAccessRefreshToken rsp = client.toBlocking().retrieve(request, BearerAccessRefreshToken.class);
